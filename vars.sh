14a88ce935b20e37ce8d83d74f61557a84b4c3c0
Laurent Nominé <laurent.nomine@gmail.com>
2025-05-09 14:49:13 +0200

    Add password generation packages

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update ; apt install -y ipcalc curl
+apt update ; apt install -y ipcalc curl whois pwgen
 
 interface=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 allips=$(ip -4 -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr}; done)
@@ -10,4 +10,3 @@ netmask=$(ipcalc ${link} | grep Netmask | awk '{ print $2 }')
 gateway=$(ip route show default | awk '/default/ { print $3 }')
 dns=$(grep nameserver /etc/resolv.conf | egrep -v "\#|:" | head -1 | awk '{ print $2 }')
 disk=$(lsblk -d | grep disk | awk '{ print $1 }')
-password=$(cat /tmp/password 2>/dev/null)
